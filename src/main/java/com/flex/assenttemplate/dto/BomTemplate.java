3483d7e7e69ca7ce35e0b4e25ac756cb2772ae54
Nithin R <Nithin.R@flex.com>
2021-09-15 15:01:51 +0530

    added dto

---
+++ src/main/java/com/flex/assenttemplate/dto/BomTemplate.java
@@ -0,0 +1,105 @@
+package com.flex.assenttemplate.dto;
+
+public class BomTemplate {
+
+	private String flexPartNo;
+	private String description;
+	private String manufacturer;
+	private String mCODE;
+	private String mPN;
+	private String quantity;
+	private String emailID;
+	private String telno;
+	private String assemblyNo;
+	private String globalMFR;
+	private String obsolete;
+
+	public String getFlexPartNo() {
+		return flexPartNo;
+	}
+
+	public void setFlexPartNo(String flexPartNo) {
+		this.flexPartNo = flexPartNo;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getManufacturer() {
+		return manufacturer;
+	}
+
+	public void setManufacturer(String manufacturer) {
+		this.manufacturer = manufacturer;
+	}
+
+	public String getmCODE() {
+		return mCODE;
+	}
+
+	public void setmCODE(String mCODE) {
+		this.mCODE = mCODE;
+	}
+
+	public String getmPN() {
+		return mPN;
+	}
+
+	public void setmPN(String mPN) {
+		this.mPN = mPN;
+	}
+
+	public String getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(String quantity) {
+		this.quantity = quantity;
+	}
+
+	public String getEmailID() {
+		return emailID;
+	}
+
+	public void setEmailID(String emailID) {
+		this.emailID = emailID;
+	}
+
+	public String getTelno() {
+		return telno;
+	}
+
+	public void setTelno(String telno) {
+		this.telno = telno;
+	}
+
+	public String getAssemblyNo() {
+		return assemblyNo;
+	}
+
+	public void setAssemblyNo(String assemblyNo) {
+		this.assemblyNo = assemblyNo;
+	}
+
+	public String getGlobalMFR() {
+		return globalMFR;
+	}
+
+	public void setGlobalMFR(String globalMFR) {
+		this.globalMFR = globalMFR;
+	}
+
+	public String getObsolete() {
+		return obsolete;
+	}
+
+	public void setObsolete(String obsolete) {
+		this.obsolete = obsolete;
+	}
+
+}
---
+++ src/main/java/com/flex/assenttemplate/dto/ManufacturerCodes.java
@@ -0,0 +1,33 @@
+package com.flex.assenttemplate.dto;
+
+public class ManufacturerCodes {
+	
+	private String global;
+	private String globalManufacturerName;
+	private String obsolete;
+
+	public String getGlobal() {
+		return global;
+	}
+
+	public void setGlobal(String global) {
+		this.global = global;
+	}
+
+	public String getGlobalManufacturerName() {
+		return globalManufacturerName;
+	}
+
+	public void setGlobalManufacturerName(String globalManufacturerName) {
+		this.globalManufacturerName = globalManufacturerName;
+	}
+
+	public String getObsolete() {
+		return obsolete;
+	}
+
+	public void setObsolete(String obsolete) {
+		this.obsolete = obsolete;
+	}
+
+}
