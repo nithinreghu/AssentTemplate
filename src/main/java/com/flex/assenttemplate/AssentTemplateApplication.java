7d80fc8fa0ae022c6c96bf254050fc8e24280462
Nithin R <Nithin.R@flex.com>
2021-09-15 14:39:24 +0530

    first commit

---
+++ src/main/java/com/flex/assenttemplate/AssentTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.flex.assenttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssentTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssentTemplateApplication.class, args);
+	}
+
+}
